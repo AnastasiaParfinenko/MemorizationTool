2b675ca0dd2eccccb660cea832ee9b2eb50c06d1
Anastasia Parfinenko <anastasia.s.parfinenko@gmail.com>
2024-11-27 13:36:07 +0100

    Implement second step
    First work with SQLAlchemy

--- tool.py
+++ tool.py
@@ -1,8 +1,28 @@
 import sys
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Flashcard(Base):
+    __tablename__ = 'flashcards'
+
+    id = Column(Integer, primary_key=True)
+    question = Column(String, nullable=False)
+    answer = Column(String, nullable=False)
+
+
+engine = create_engine('sqlite:///flashcard.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
 
 main_menu = {'1': 'Add flashcards', '2': 'Practice flashcards', '3': 'Exit'}
 add_menu = {'1': 'Add a new flashcard', '2': 'Exit'}
-flashcards = {}
 
 
 def print_menu(menu):
@@ -45,17 +65,22 @@ def add_flashcards():
     a = ''
     while not a:
         a = input('Answer:\n').strip()
-    flashcards[q] = a
+
+    new_card = Flashcard(question=q, answer=a)
+    session.add(new_card)
+    session.commit()
+
     work_add_menu()
 
 
 def work_practice():
+    flashcards = session.query(Flashcard).all()
     for card in flashcards:
         print()
-        print(f'Question: {card}')
+        print(f'Question: {card.question}')
         y_or_n = input('Please press "y" to see the answer or press "n" to skip:\n')
         if y_or_n == 'y':
-            print(f'Answer: {flashcards[card]}')
+            print(f'Answer: {card.answer}')
         elif y_or_n == 'n':
             continue
 
